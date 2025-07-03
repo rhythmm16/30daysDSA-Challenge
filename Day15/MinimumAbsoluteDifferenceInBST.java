a6c047a3959357043ee1b679d90c877c4ffa4626
rhythmm16 <rhythm161203@gmail.com>
2025-07-03 08:58:02 +0530

    binarysearchtree

---
+++ Day15/MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,22 @@
+class Solution {
+    Integer prev = null;
+    int minDiff = Integer.MAX_VALUE;
+
+    public int getMinimumDifference(TreeNode root) {
+        inorder(root);
+        return minDiff;
+    }
+
+    private void inorder(TreeNode node) {
+        if (node == null) return;
+
+        inorder(node.left);
+
+        if (prev != null) {
+            minDiff = Math.min(minDiff, node.val - prev);
+        }
+        prev = node.val;
+
+        inorder(node.right);
+    }
+}
