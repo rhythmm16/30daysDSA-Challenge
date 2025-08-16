92c85902b4762a801e836c7b1e0f14292262da22
rhythmm16 <156988775+rhythmm16@users.noreply.github.com>
2025-08-16 20:31:13 +0530

    Create maximum69.java

---
+++ Day59/maximum69.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximum69Number (int num) {
+        String str = Integer.toString(num); 
+        StringBuilder sb = new StringBuilder(str);
+
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '6') {
+                sb.setCharAt(i, '9'); 
+                break; 
+            }
+        }
+
+        return Integer.parseInt(sb.toString());
+    }
+}
