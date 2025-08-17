c7bb90d45cc1f638c7310b34819c4eacb2862adc
rhythmm16 <156988775+rhythmm16@users.noreply.github.com>
2025-08-17 18:57:40 +0530

    Create 3SumClosest.java

---
+++ Day60/3SumClosest.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int closestSum = nums[0] + nums[1] + nums[2]; // initial guess
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1, right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                // Update closest if better
+                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                    closestSum = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    // Exact match
+                    return sum;
+                }
+            }
+        }
+        return closestSum;
+    }
+}
