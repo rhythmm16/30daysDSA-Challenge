a0f3a594523db3d0cd0549d1af6bc59b4a325d12
rhythmm16 <rhythm161203@gmail.com>
2025-08-20 10:52:35 +0530

    daily

---
+++ Day63/CountSquareSubmatricesWithAllOnes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int total = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1; // first row/col can only form size-1 squares
+                    } else {
+                        dp[i][j] = 1 + Math.min(
+                            dp[i - 1][j],
+                            Math.min(dp[i][j - 1], dp[i - 1][j - 1])
+                        );
+                    }
+                    total += dp[i][j]; // add all squares ending at (i, j)
+                }
+            }
+        }
+        return total;
+    }
+}
