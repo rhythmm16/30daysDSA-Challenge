54c2cc8efaca81fc776c2479493b69f962ca0f23
rhythmm16 <rhythm161203@gmail.com>
2025-08-30 09:09:48 +0530

    dailyquestion

---
+++ Day73/ValidSudoku.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {        // rows
+            for (int j = 0; j < 9; j++) {    // columns
+                char num = board[i][j];
+                if (num != '.') {
+                    // encode uniqueness conditions
+                    String rowKey = num + " in row " + i;
+                    String colKey = num + " in col " + j;
+                    String boxKey = num + " in box " + (i/3) + "-" + (j/3);
+
+                    // if any duplicate is found, board is invalid
+                    if (!seen.add(rowKey) || !seen.add(colKey) || !seen.add(boxKey)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
