216e858502870bdec80cc86ad23c4823b080e60c
rhythmm16 <rhythm161203@gmail.com>
2025-09-02 21:36:31 +0530

    issue fix

--- Day76/FindNumberOfWaysToPlacePeople1.java
+++ Day76/FindNumberOfWaysToPlacePeople1.java
@@ -3,12 +3,15 @@ class Solution {
         int n = points.length, count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (points[i][0] < points[j][0] && points[i][1] > points[j][1]) {
+                int x1 = points[i][0], y1 = points[i][1];
+                int x2 = points[j][0], y2 = points[j][1];
+                
+                if (x1 <= x2 && y1 >= y2 && (x1 < x2 || y1 > y2)) {
                     boolean valid = true;
                     for (int k = 0; k < n; k++) {
                         if (k == i || k == j) continue;
-                        if (points[k][0] >= points[i][0] && points[k][0] <= points[j][0] &&
-                            points[k][1] <= points[i][1] && points[k][1] >= points[j][1]) {
+                        int x = points[k][0], y = points[k][1];
+                        if (x >= x1 && x <= x2 && y <= y1 && y >= y2) {
                             valid = false;
                             break;
                         }
