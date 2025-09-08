a75ad00562d4026db756da3517380b51ec88e0ee
rhythmm16 <rhythm161203@gmail.com>
2025-09-08 08:59:43 +0530

    Daily Question

---
+++ Day82/ConvertIntegerToSumOfTwoNoZeroIntegers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] getNoZeroIntegers(int n) {
+        for (int a = 1; a < n; a++) {
+            int b = n - a;
+            if (isNoZero(a) && isNoZero(b)) {
+                return new int[]{a, b};
+            }
+        }
+        return new int[]{};
+    }
+
+    private boolean isNoZero(int x) {
+        while (x > 0) {
+            if (x % 10 == 0) return false;
+            x /= 10;
+        }
+        return true;
+    }
+}
