5fb2af6bfffbc62868e2343ce053d164dc346be6
rhythmm16 <rhythm161203@gmail.com>
2025-11-13 11:02:31 +0530

    daily question

---
+++ Day146/MaximumNumberOfOperationsToMoveOnesToEnd.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxOperations(String s) {
+        int ones = 0;
+        long ans = 0; 
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '1') {
+                ones++;
+            } else { 
+
+                if (i == n - 1 || s.charAt(i + 1) == '1') {
+                    ans += ones;
+                }
+            }
+        }
+
+        return (int) ans;
+    }
+}
